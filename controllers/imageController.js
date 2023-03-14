a258fed603f029a89b1910f828b35f55e6684f82
Davidchapman777 <105242209+Davidchapman777@users.noreply.github.com>
2023-03-14 11:40:21 -0400

    fourth

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -5,24 +5,24 @@ import { v2 as cloudinary } from "cloudinary"
 import fs from 'fs'
 
 
-const uploadImage = async (req, res) => {
-    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
-        use_filename: true,
-        folder:'catch-of-the-day'
-    })
-    fs.unlinkSync(req.files.image.tempFilePath)
-    return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
-}
-export { uploadImage }
-
-
-
-
-
 // const uploadImage = async (req, res) => {
-//     const postImage = req.files.image;
-//     const imagePath = path.join(__dirname, './public/uploads/' + `${postImage.name}`)
-//     await postImage.mv(imagePath)
-//     res.status(StatusCodes.OK).json({image:{src:`/uploads/${postImage.name}`}})
+//     const result = await cloudinary.uploader.upload(req.files.image.tempFilePath, {
+//         use_filename: true,
+//         folder:'catch-of-the-day'
+//     })
+//     fs.unlinkSync(req.files.image.tempFilePath)
+//     return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
 // }
-// export {uploadImage}
+// export { uploadImage }
+
+
+
+
+
+const uploadImage = async (req, res) => {
+    const postImage = req.files.image;
+    const imagePath = path.join(__dirname, './public/uploads/' + `${postImage.name}`)
+    await postImage.mv(imagePath)
+    res.status(StatusCodes.OK).json({image:{src:`/uploads/${postImage.name}`}})
+}
+export {uploadImage}
